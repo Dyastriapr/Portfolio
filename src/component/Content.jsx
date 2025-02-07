80b59ce84dfee5f764e34bfa4ca697484cdbd56e
Dyastriapr <dyastriap1@gmail.com>
2025-02-07 11:10:06 +0700

    uplaod

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -15,13 +15,13 @@ export default function Content() {
         <div className="shadow-lg rounded-bl-xl rounded-tr-xl overflow-hidden">
           <div className="flex">
             <div
-              className={`px-6 py-2 cursor-pointer transition-all ${activeTab === "about" ? "bg-black text-white shadow-md" : "text-white bg-black"}`}
+              className={`px-6 py-2 cursor-pointer transition-all ${activeTab === "about" ? "bg-black text-white shadow-md" : "text-white bg-base-100"}`}
               onClick={() => setActiveTab("about")}
             >
               About
             </div>
             <div
-              className={`px-6 py-2 cursor-pointer transition-all ${activeTab === "project" ? "bg-black text-white shadow-md" : "text-white bg-black"}`}
+              className={`px-6 py-2 cursor-pointer transition-all ${activeTab === "project" ? "bg-black text-white shadow-md" : "text-white bg-base-100"}`}
               onClick={() => setActiveTab("project")}
             >
               Project
@@ -34,22 +34,22 @@ export default function Content() {
       <div className="p-4">
         {activeTab === "about" ? (
           <div>
-            <div className="text-2xl font-bold">About Me</div>
+            <div className="text-2xl font-bold text-white">About Me</div>
             <div className="divider divider-neutral"></div>
             <p className="text-black">
               Saya adalah seorang Programmer Website yang solid dalam pengembangan web, baik di sisi Frontend. Saya memiliki minat yang mendalam dalam menciptakan aplikasi web yang responsif, intuitif, dan ramah pengguna. Selain itu, saya juga tertarik dalam pengembangan Kecerdasan Buatan (AI), dan terus memperluas pengetahuan saya di bidang ini untuk mengintegrasikan teknologi AI ke dalam aplikasi web modern. Dengan kombinasi keahlian dalam pemrograman dan minat terhadap inovasi teknologi, saya selalu berusaha untuk memberikan solusi yang efektif dan inovatif dalam setiap proyek yang saya kerjakan.
             </p>
 
             <div className="mt-5">
-              <div className="text-xl font-bold">Minat Saya</div>
+              <div className="text-xl font-bold text-black">Minat Saya</div>
               <div className="flex flex-wrap justify-center gap-5 md:flex-nowrap">
-                <div className="card bg-base-100 w-96 shadow-2xl">
-                  <div className="card-body">
+                <div className="card bg-white w-96 shadow-2xl">
+                  <div className="card-body text-black">
                     <div className="font-bold">Web Design</div>
                     <p>Website yang dirancang dengan mempertimbangkan UI/UX, menawarkan antarmuka yang modern dan interaktif.</p>
                   </div>
                 </div>
-                <div className="card bg-base-100 w-96 shadow-2xl">
+                <div className="card bg-white shadow-2xl  text-black">
                   <div className="card-body">
                     <div className="font-bold">Kecerdasan Buatan</div>
                     <p>Saya minat diberbagai jenis teknologi yang berkaitan dengan AI seperti Chatbot, Machine Learning, Deep Learning, dan lain-lain.</p>
